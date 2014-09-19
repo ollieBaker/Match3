2f6a5e659807d8e3a0efd99f08dd649ea6ae5705
Ollie Baker <oliverjohnbaker@gmail.com>
2014-09-19 12:09:50 +0100

    Initial Commit

---
+++ Assets/Grid.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Grid : MonoBehaviour {
+
+	public float spacing = 0.1f;
+	public float tileSize = 1f;
+	public GameObject tilePrefab;
+	private List<GameObject> grid;
+	public int columns = 7;
+	public int rows = 7;
+
+	// Use this for initialization
+	void Start () {
+		grid = new List<GameObject>( );
+		for (int i = 0; i < columns; i++) {
+			for (int j = 0; j < rows; j++) {
+				GameObject t = Instantiate(tilePrefab, new Vector3(
+					(i * tileSize) + (spacing * i) + (spacing/2), 
+					-(j * tileSize) - (spacing * j) - (spacing/2), 
+					0), Quaternion.identity) as GameObject;
+				grid.Add(t);
+				Tile ts = (Tile) t.GetComponent(typeof(Tile));
+				ts.init(0, i, j);
+				//grid[i * columns + j] = t;
+			}
+		}
+	}
+
+	GameObject retrieve(int x, int y) {	
+		GameObject item = null;
+		if (x >= 0 && y >= 0 && x < columns && y < rows) {
+			item = grid[x * columns + y];
+		}
+		return item;
+	}
+
+	public void setNewGridPosition(GameObject tile, int x, int y) {	
+		Tile ts = (Tile) tile.GetComponent(typeof(Tile));
+		grid[x * columns + y] = tile;	
+		ts.column = x;
+		ts.row = y;
+		
+		//retrieve(x,y).filters=[new GlowFilter(getColour(),1,10,10,5)]
+	}
+	
+	public void swapItems(GameObject selectedItem, GameObject target) {
+		int a = grid.IndexOf(selectedItem);
+		int b = grid.IndexOf(target);
+		
+		grid[a] = target;
+		grid[b] = selectedItem;	
+	}
+
+	public void resortAll() 
+	{
+		List<GameObject> newOrder = new List<GameObject>();
+		//newOrder.length = 49;
+		
+		for (int i = 0; i < 0; i++ ) 
+		{			
+			GameObject item = grid[i];
+			if(item != null) {
+				Tile ts = (Tile) item.GetComponent(typeof(Tile));
+				newOrder[ts.column * columns + ts.row] = item;
+			}
+		}			
+		grid = new List<GameObject>();
+		grid.AddRange(newOrder);				
+	}
+	
+	public void removeItem(int column, int row) 
+	{
+		GameObject item = retrieve(column, row);			
+		if(item != null) {
+			//removeChild(item);
+			//item.destroy();
+			//item = null;
+			//grid[column * columns + row] = null;
+		}
+	}		
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Tile.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	[SerializeField]
+	private int id;
+	private int size;
+	private int colour;
+	public int row;
+	public int column;
+	private Vector2 dir;
+	private Material material;
+	public GameObject tile;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	public void init (int id, int col, int row) {
+		this.id = id;
+		this.column = col;
+		this.row = row;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
